7a5b72bc649a85cf9e3b3fdd51a5d9badf5d3049
Marios Kifokeris <m.kifokeris@thetccgroup.org>
2025-11-28 10:25:22 +0200

    Add Vercel configuration and upgrade to Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react()],
+  adapter: vercel(),
 });
 
